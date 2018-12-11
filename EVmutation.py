d954e8bd785c9f4ff226c5ea8ab2ace90189d56b
luponzo86 <ponzoniluca@gmail.com>
2018-12-11 14:44:50 -0500

    EVmutation: read files line by line and stop after finding mutants

--- EVmutation.py
+++ EVmutation.py
@@ -3,52 +3,17 @@ from os import listdir
 from os.path import splitext, join
 from prody import LOGGER
 
+# extract precomputed EVmutation scores for given mutants
+# NB:
+# negative DeltaE_epist --> deleterious effect
+# DeltaE_epist == 0     --> neutral effect (wild-type)
+# positive DeltaE_epist --> neutral/benign effect 
+
 
 EVMUT_FOLDER = '/home/lponzoni/Data/025-EVmutation/mutation_effects/'
 
 EVMUT_FEATS = ['EVmut-DeltaE_epist', 'EVmut-DeltaE_indep',
                'EVmut-mut_aa_freq', 'EVmut-wt_aa_cons',]
-# EVMUT_FEATS += ['EVmut-DeltaE_epist_rank',
-#                 'EVmut-DeltaE_epist_rank2',
-#                 'EVmut-DeltaE_epist_norm',
-#                 'EVmut-DeltaE_epist_norm2',]
-
-
-def _calcEVmutFeatures(data, wt_aa, mut_aa):
-    n = len(data)
-    assert n==20, 'Incorrect number of mutants: {} instead of 20.'.format(n)
-    assert set(data[:,3])==set('ACDEFGHIKLMNPQRSTVWY'), \
-           'Invalid set of mutants'
-    index_wt  = np.where(data[:,3]==wt_aa )[0][0]
-    index_mut = np.where(data[:,3]==mut_aa)[0][0]
-    # extract precomputed EVmut scores for given mutant
-    precomputed_scores = data[index_mut, 4:8]
-#     # compute normalized/ranked version of DeltaE_epist:
-#     # negative DeltaE --> 1 (deleterious effect)
-#     # DeltaE = 0 (wt) --> ? (neutral effect)
-#     # positive DeltaE --> 0 (neutral/benign effect) 
-#     DeltaE_epist = np.array(data[:, 4], dtype=float)
-#     ### normalization
-#     norm = np.ptp(DeltaE_epist)
-#     DeltaE_epist_norm  = - DeltaE_epist[index_mut]/norm #[wt=0]
-#     DeltaE_epist_norm2 = -(DeltaE_epist[index_mut]-np.max(DeltaE_epist))/norm
-#     ### ranking
-#     array = -DeltaE_epist
-#     order = array.argsort()
-#     ranks = order.argsort()
-#     DeltaE_epist_rank  =  ranks[index_mut]/19.
-#     DeltaE_epist_rank2 = (ranks[index_mut]-ranks[index_wt])/19. #[wt=0]
-#     # output
-#     feats = np.empty(8)
-#     feats[:4] = precomputed_scores
-#     feats[4]  = DeltaE_epist_rank
-#     feats[5]  = DeltaE_epist_rank2
-#     feats[6]  = DeltaE_epist_norm
-#     feats[7]  = DeltaE_epist_norm2
-    # output
-    feats = np.empty(4)
-    feats[:] = precomputed_scores
-    return feats
 
 
 def recoverEVmutFeatures(SAVs):
@@ -75,36 +40,25 @@ def recoverEVmutFeatures(SAVs):
     for i, SAV in enumerate(SAVs):
         acc, pos, wt_aa, mut_aa, SAV_txt = SAV
 #       LOGGER.info('Recovering EVmutation data for {}.'.format(SAV_txt))
-        err_msg = "EVmutation data not found for '{}'".format(SAV_txt)
         # find files containing given SAV coordinates
         match_files = find_matching_files(file_list, acc, pos)
-        if len(match_files) == 0:
-#           LOGGER.warn(err_msg)
-            continue
         # recover data and average them if multiple values are found
-        feat_arrays = []
+        mutant = f'{wt_aa}{pos}{mut_aa}'
+        data = []
         for fname in match_files:
-            full_path = join(EVMUT_FOLDER, fname)
-            data = np.genfromtxt(full_path, delimiter=';', dtype=str)
-            # find entries for requested position
-            pos_str = str(pos)
-            data = data[data[:,1]==pos_str]
-            if len(data) == 0:
-                continue
-            # extract/compute features
-            try:
-                arr = _calcEVmutFeatures(data, wt_aa, mut_aa)
-            except Exception as e:
-                LOGGER.warn(str(e))
-                continue
-            else:
-                feat_arrays.append(arr)
-        if feat_arrays:
-            avg_array = np.mean(feat_arrays, axis=0)
-            features[i] = tuple(avg_array)
-        else:
-#           LOGGER.warn(err_msg)
+            with open(join(EVMUT_FOLDER, fname), 'r') as f:
+                for line in f:
+                    if line.startswith(mutant):
+                        l = line.strip().split(';')[4:8]
+                        data.append(l)
+                        break
+        data = np.array(data, dtype=float)
+        if len(data) == 0:
+#           LOGGER.warn(f"EVmutation data not found for '{SAV_txt}'")
             continue
+        else:
+            features[i] = tuple(np.mean(data, axis=0))
+
     LOGGER.report('EVmutation scores recovered in %.1fs.', '_EVmut')
     return features 
 
