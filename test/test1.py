7958d80c1d558f9fc4eb2950f80f0b8b10926d6c
luponzo86 <ponzoniluca@gmail.com>
2019-01-15 18:34:38 -0500

    -

--- test/test1.py
+++ test/test1.py
@@ -1,10 +1,7 @@
 import sys
 sys.path.append('../rhapsody')
+
+# just check if rhapsody can be imported correctly
 from rhapsody import *
 
-# EXAMPLE
-def func(x):
-    return x + 1
-
-def test_answer():
-    assert func(3) == 5
+assert True
