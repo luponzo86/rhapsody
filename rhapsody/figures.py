37e95ec5522650ae69b2028c5b7e232d9c959c58
luponzo86 <ponzoniluca@gmail.com>
2019-01-17 23:27:27 -0500

    created figures.py file where all figure function will be collected

---
+++ rhapsody/figures.py
@@ -0,0 +1,14 @@
+import matplotlib
+from rhapsody import Rhapsody
+
+# def foo():
+#     try:
+#         import external_module
+#     except ImportError:
+#         external_module = None
+#
+#     if external_module:
+#         external_module.some_whizzy_feature()
+#     else:
+#         print("You could be using a whizzy feature right now, \
+#         if you had external_module.")
