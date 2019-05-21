7c614ab85433764142249cea968fbf2605115116
shz66 <shz66@pitt.edu>
2019-05-21 15:16:50 -0400

    update version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='prody-rhapsody',
-    version='1.0.0',
+    version='0.9.0',
     description='Python program, based on ProDy, for pathogenicity prediction of human missense variants.',  
     long_description=long_description, 
     long_description_content_type='text/markdown', 
