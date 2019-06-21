d0821762310c469ecaa5c6ecf81c9b0bc698349f
Luca Ponzoni <16854208+luponzo86@users.noreply.github.com>
2019-06-20 21:55:35 -0400

    -

--- docs/conf.py
+++ docs/conf.py
@@ -58,15 +58,15 @@ html_theme_options = {
     'canonical_url': '',
     # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'logo_only': False,
-    'display_version': True,
+    'display_version': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
+    'style_nav_header_background': '#b7270b',
     # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
+    'collapse_navigation': False,
+    'sticky_navigation': False,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': True
 }
